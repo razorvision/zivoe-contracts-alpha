e9793c8bc789cfd1823cf554e412ed7400f3437e
Daniel Hedrick <daniel.hedrick@razorvision.net>
2021-11-30 13:56:19 -0700

    ZVE and sZVE token contracts

---
+++ contracts/ZivoeERC20Token.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+import "./libs/TWAPOracleUpdater.sol";
+
+//modified from OlympusDAO
+//author : dcoder2099
+
+contract ZivoeERC20Token is TWAPOracleUpdater {
+
+    using SafeMath for uint256;
+	
+    constructor() TWAPOracleUpdater("ZivoeDAO", "ZVE", 9) {
+    }
+
+    function mint(address account_, uint256 amount_) external onlyVault() {
+        _mint(account_, amount_);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(msg.sender, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account_, uint256 amount_) public virtual {
+        _burnFrom(account_, amount_);
+    }
+
+    function _burnFrom(address account_, uint256 amount_) public virtual {
+        uint256 decreasedAllowance_ =
+            allowance(account_, msg.sender).sub(
+                amount_,
+                "ERC20: burn amount exceeds allowance"
+            );
+
+        _approve(account_, msg.sender, decreasedAllowance_);
+        _burn(account_, amount_);
+    }
+}
